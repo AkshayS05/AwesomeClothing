8efbed05d51bb85accc5d54b48ec2539d5f454ea
akshay <akshaysharma5432@gmail.com>
2022-09-21 08:20:56 +0530

    add products

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../Components/Navbar/Navbar';
 import Announcement from '../../Components/Announcement/Announcement';
 import Slider from '../../Components/Slider/Slider';
 import Categories from '../../Components/Categories/Categories';
+import Products from '../../Components/Products/Products';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 import Fashion from './assets/clothing.png';
 import Shoes from './assets/shoes.png';
 import Purse from './assets/purse.png';
-import { PausePresentation } from '@mui/icons-material';
+
 export const sliderItems = [
   {
     id: 1,
@@ -42,5 +42,40 @@ export const categories = [
     id: 3,
     img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     title: 'LIGHT JACKETS',
+  },
+];
+//products
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
+  },
+  {
+    id: 2,
+    img: 'https://m.media-amazon.com/images/I/319w3c3OqNL.jpg',
+  },
+  {
+    id: 3,
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/41SlXVpgFlS._SY445_SX342_QL70_FMwebp_.jpg',
+  },
+  {
+    id: 4,
+    img: 'https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png',
+  },
+  {
+    id: 5,
+    img: 'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
+  },
+  {
+    id: 6,
+    img: 'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
+  },
+  {
+    id: 7,
+    img: 'https://m.media-amazon.com/images/I/71cFpnm0b6S._UX679_.jpg',
+  },
+  {
+    id: 8,
+    img: 'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
   },
 ];
