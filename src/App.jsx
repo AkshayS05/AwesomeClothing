7c4a65983c79b0e411348a730ad131dbb7a9ba9b
akshay <akshaysharma5432@gmail.com>
2022-09-23 06:10:52 +0530

    add login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Product from './Pages/Product/Product';
 import ProductList from './Pages/ProductList/ProductList';
+import Register from './Pages/Register/Register';
 const App = () => {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 };
